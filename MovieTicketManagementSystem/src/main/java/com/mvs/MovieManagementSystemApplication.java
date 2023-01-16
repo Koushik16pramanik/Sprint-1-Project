c3426c8d49995ef671a19a13de1f109cdde7a798
Koushik Pramanik <110465853+Koushik16pramanik@users.noreply.github.com>
2023-01-16 22:06:04 +0530

    Add files via upload

--- MovieTicketManagementSystem/src/main/java/com/mvs/MovieManagementSystemApplication.java
+++ MovieTicketManagementSystem/src/main/java/com/mvs/MovieManagementSystemApplication.java
@@ -9,5 +9,4 @@ public class MovieManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieManagementSystemApplication.class, args);
 	}
-
 }
--- MovieTicketManagementSystem/src/main/java/com/mvs/serviceImpl/MovieServiceImpl.java
+++ MovieTicketManagementSystem/src/main/java/com/mvs/serviceImpl/MovieServiceImpl.java
@@ -8,32 +8,38 @@ import org.springframework.stereotype.Service;
 import com.mvs.entity.Movie;
 import com.mvs.repository.MovieRepo;
 import com.mvs.service.MovieService;
+
 @Service
 public class MovieServiceImpl implements MovieService {
 
 	@Autowired
-	private MovieRepo repo;
+	private MovieRepo movieRepo;
+
 	public Movie saveMovie(Movie movie) {
-		return repo.save(movie);
+		return movieRepo.save(movie);
 	}
-	public List<Movie> getMovies(){
-		return repo.findAll();
+
+	public List<Movie> getMovies() {
+		return movieRepo.findAll();
 	}
+
 	public Movie getMovieByName(String movieName) {
-		return repo.findById(movieName).orElse(null);
+		return movieRepo.findById(movieName).orElse(null);
 	}
+
 	public Movie updateMovie(Movie movie) {
-		Movie existingMovie=repo.findById(movie.getMovieName()).orElse(null);
+		Movie existingMovie = movieRepo.findById(movie.getMovieName()).orElse(null);
 		existingMovie.setMovieName(movie.getMovieName());
 		existingMovie.setMultiplex(movie.getMultiplex());
 		existingMovie.setScreen(movie.getScreen());
 		existingMovie.setSeatCount(movie.getSeatCount());
 		existingMovie.setTime(movie.getTime());
 		existingMovie.setTicketPrice(movie.getTicketPrice());
-		return repo.save(existingMovie);
+		return movieRepo.save(existingMovie);
 	}
+
 	public String deleteMovie(String movieName) {
-		repo.deleteById(movieName);
-		return "Movie deleted with name : "+movieName;
+		movieRepo.deleteById(movieName);
+		return "Movie deleted with name : " + movieName;
 	}
 }
